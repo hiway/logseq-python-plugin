4159a6513ad1075715a7da0a7a46dae45fcc75c1
Harshad Sharma <harshad@sharma.io>
2022-10-14 23:39:48 +0530

    Reset.

--- src/logseq_plugin/app.py
+++
@@ -1,6 +0,0 @@
-class LogseqApp(object):
-    def __init__(self, plugin):
-        self._plugin = plugin
-
-    async def showMsg(self, message: str, type: str = "success"):
-        await self._plugin.emit("logseq.App.showMsg", (message, type))
