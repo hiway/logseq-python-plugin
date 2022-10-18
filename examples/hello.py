3785ac4817103b1b85c5469c67dd0398bfa24355
Harshad Sharma <harshad@sharma.io>
2022-10-18 20:22:53 +0530

    Cleanup examples

---
+++ examples/hello.py
@@ -0,0 +1,12 @@
+from logspyq.api import LogseqPlugin
+
+logseq = LogseqPlugin(name="Hello Agent", description="Say hello to Logseq")
+
+
+@logseq.Editor.registerSlashCommand("hello")
+async def hello(sid):
+    await logseq.Editor.insertAtEditingCursor("Hello, World!")
+
+
+if __name__ == "__main__":
+    logseq.run(host="localhost", port=3000)
--- src/logspyq/agents/bye.py
+++
@@ -1,7 +0,0 @@
-from logspyq.api import LogseqPlugin
-
-logseq = LogseqPlugin(name="Bye Agent", description="Say bye to Logseq")
-
-@logseq.Editor.registerSlashCommand("bye")
-async def bye(sid):
-    await logseq.Editor.insertAtEditingCursor("Seeya later, World!")
