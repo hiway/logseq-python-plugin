552a4173d3c7d23cab0b80d3aadd98a3f21844e4
Harshad Sharma <harshad@sharma.io>
2022-10-19 06:34:42 +0530

    Add draft unix-shell agent

---
+++ src/logspyq/agents/unixshell/__init__.py
@@ -0,0 +1,71 @@
+import time
+from logspyq.api import LogseqPlugin
+from .lib import shellcheck_command, execute_command_async
+
+
+logseq = LogseqPlugin(
+    name="UNIX Shell", description="Execute a UNIX shell command from Logseq"
+)
+
+
+@logseq.App.registerCommandPalette(
+    "cmdexec", binding="ctrl+enter", label="Execute in System Shell"
+)
+async def execute_command_in_shell(sid, event):
+    """
+    Execute content of current block as command in the system shell.
+    Use shellcheck to validate the command.
+    """
+    command = await logseq.Editor.getEditingBlockContent()
+    current_block = await logseq.Editor.getCurrentBlock()
+
+    if not command:
+        await logseq.App.showMsg("No command to execute!", "warning", timeout=3000)
+        return
+
+    # Run shellcheck
+    shellcheck_results = await shellcheck_command(command)
+    if shellcheck_results:
+        await logseq.App.showMsg(
+            "Shellcheck found issues with the command."
+        )
+        results = "\n".join([str(r) for r in shellcheck_results])
+        await logseq.Editor.insertBlock(
+            current_block.uuid, f"```stderr\n{results}\n```", sibling=False
+        )
+        return
+    # Execute command
+    start_time = time.time()
+    result = await execute_command_async(command)
+    duration = time.time() - start_time
+    if result.timeout:
+        await logseq.App.showMsg(
+            f"Command timed out. Exit code: {result.exitcode}", "warning", timeout=3000
+        )
+        return
+    # Insert stdout and stderr as new blocks
+    properties = {
+        "exit_code": result.returncode,
+        "start_time": time.strftime("%Y-%m-%d %H:%M", time.localtime(start_time)),
+        "duration": f"{duration:.3f}",
+    }
+    if result.stderr:
+        await logseq.Editor.insertBlock(
+            current_block.uuid,
+            "```stderr\n" + result.stderr + "\n```",
+            sibling=False,
+            properties=properties,
+        )
+    if result.stdout:
+        await logseq.Editor.insertBlock(
+            current_block.uuid,
+            "```stdout\n" + result.stdout + "\n```",
+            sibling=False,
+            properties=properties,
+        )
+    if not any([result.stdout, result.stderr]):
+        await logseq.App.showMsg("Command executed successfully.", timeout=3000)
+
+
+if __name__ == "__main__":
+    logseq.run(host="localhost", port=8484, debug=True)
---
+++ src/logspyq/agents/unixshell/lib.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+import os
+import subprocess
+from typing import List, Optional
+from box import Box
+
+logger = logging.getLogger(__name__)
+SHELLCHECK_PATH = os.environ.get("SHELLCHECK_PATH", "shellcheck")
+
+
+async def shellcheck_command(
+    command: str,
+    shell: str = "bash",
+    extra_args: Optional[List[str]] = None,
+) -> List[str]:
+    """
+    Run shellcheck on a given string.
+    """
+    # Setup shellcheck command
+    shellcheck_command = [SHELLCHECK_PATH, "-s", shell, "-x", "-"]
+    if extra_args:
+        shellcheck_command.extend(extra_args)
+
+    # Run shellcheck
+    logger.debug(f"Running shellcheck command: {shellcheck_command}")
+    process = await asyncio.create_subprocess_exec(
+        *shellcheck_command,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate(command.encode("utf-8"))
+    logger.debug(f"Shellcheck stdout: {stdout}")
+    logger.debug(f"Shellcheck stderr: {stderr}")
+
+    # Parse shellcheck output
+    shellcheck_results = []
+    for line in stdout.decode("utf-8").splitlines():
+        if not line:
+            continue
+        shellcheck_results.append(line)
+        continue
+
+    return shellcheck_results
+
+
+async def execute_command_async(command: str, timeout: float = 10) -> Box:
+    """
+    Execute a command asynchronously.
+    """
+    command = command.split(" ")  # type: ignore
+
+    logger.debug(f"Running command: {command}")
+    process = await asyncio.create_subprocess_exec(
+        *command,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    result = Box()
+    try:
+        stdout, stderr = await asyncio.wait_for(process.communicate(), timeout)
+        result.timeout = False
+    except asyncio.TimeoutError:
+        logger.debug(f"Command timed out: {command}")
+        process.kill()
+        stdout, stderr = await process.communicate()
+        result.timeout = True
+    logger.debug(f"Command stdout: {stdout}")
+    logger.debug(f"Command stderr: {stderr}")
+
+    # Parse output
+    result.stdout = stdout.decode("utf-8").strip()
+    result.stderr = stderr.decode("utf-8").strip()
+    result.returncode = process.returncode
+    return result
